3adeebac06e886708501d0709f15b027553121ae
Nishant9091 <nishantsharma7428@gmail.com>
2025-05-09 02:00:10 +0530

    minor

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/home/Home.jsx';
-import Service from './pages/service/Service.jsx'; // Create this component
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
+import Home from './pages/home/Home.jsx';
+import Service from './pages/service/Service.jsx'; // Create this component
 import Results from './pages/results/Results.jsx';
+import './App.css';
 
 const App = () => {
   return (
